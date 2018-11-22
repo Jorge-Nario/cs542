19fbfc43336c047a4a6ce54412058b2d47a34914
jonhale1 <jon.hale1@gmail.com>
2018-11-22 08:36:56 -0500

    Add files via upload

--- bag_of_words.py
+++ bag_of_words.py
@@ -5,12 +5,14 @@ vectorizer = CountVectorizer()
 
 news_data = pd.read_csv("/Users/jonathanhale/Documents/Courses/Machine Learning/example_data/news_sample (1).csv")
 
+
+
 def bag_of_words (news_data):
-    r = news_data['headline'].shape[0]
+    """bag_of_words function to process the news 'headline' data."""
     corpus = news_data['headline'].values.tolist()
     mat_corpus = vectorizer.fit_transform(corpus).toarray()
     news_data['headline'] = list(mat_corpus)
-    print(news_data['headline'])
+    return news_data
 
 bag_of_words(news_data)
 
