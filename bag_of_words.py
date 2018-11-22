f70f17e7568c5df8b27ace01aa8baa42b105846e
jonhale1 <jon.hale1@gmail.com>
2018-11-22 08:31:56 -0500

    Add files via upload
    Improved

--- bag_of_words.py
+++ bag_of_words.py
@@ -9,9 +9,6 @@ def bag_of_words (news_data):
     r = news_data['headline'].shape[0]
     corpus = news_data['headline'].values.tolist()
     mat_corpus = vectorizer.fit_transform(corpus).toarray()
-    vec_corpus = list(range(0, r-1))
-    for i in range(0,r-1):
-        vec_corpus[i] = mat_corpus[i:]
     news_data['headline'] = list(mat_corpus)
     print(news_data['headline'])
 
